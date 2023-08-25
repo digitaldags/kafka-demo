26e71f6b4640d6535d01dc73ee834f84aa784d6c
digitaldags <janndaniel.dagusen@gmail.com>
2023-08-25 11:39:05 +0800

    Add id in kafka listener for stopping and starting

--- src/main/java/com/kafka/consumer/kafkademo/consumer/KafkaListenerComponent.java
+++ src/main/java/com/kafka/consumer/kafkademo/consumer/KafkaListenerComponent.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class KafkaListenerComponent {
 
-    @KafkaListener(topics = "test-12345", groupId = "test-group")
-    public void listenGroupFoo(String message) {
+    @KafkaListener(id = "theConsumer", topics = "test-12345", groupId = "test-group")
+    public void listen(String message) {
         System.out.println(message);
     }
 }
