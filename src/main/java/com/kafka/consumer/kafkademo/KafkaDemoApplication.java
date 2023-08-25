47e3549bdc75e883cb3fa6673169ca351df1f804
digitaldags <janndaniel.dagusen@gmail.com>
2023-08-25 11:47:22 +0800

    Add event listener that stops consumer on startup

--- src/main/java/com/kafka/consumer/kafkademo/KafkaDemoApplication.java
+++ src/main/java/com/kafka/consumer/kafkademo/KafkaDemoApplication.java
@@ -1,13 +1,39 @@
 package com.kafka.consumer.kafkademo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
+import org.springframework.kafka.listener.MessageListenerContainer;
 
 @SpringBootApplication
 public class KafkaDemoApplication {
+    @Autowired
+    private KafkaListenerEndpointRegistry endpointRegistry;
 
-	public static void main(String[] args) {
-		SpringApplication.run(KafkaDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaDemoApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationListener<ContextRefreshedEvent> onApplicationStart() {
+        return event -> {
+            stopConsumerOnStartup();
+        };
+    }
+
+    private void stopConsumerOnStartup() {
+        MessageListenerContainer endpoint = endpointRegistry.getListenerContainer("theConsumer");
+
+        if (endpoint != null) {
+            endpoint.stop();
+            System.out.println("Consumer stopped on application startup.");
+        } else {
+            System.out.println("Consumer not found.");
+        }
+    }
 
 }
